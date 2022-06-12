e3e103fc9af8376880299c1571d64539560ef960
nhaTahn <74145826+nhaTahn@users.noreply.github.com>
2022-06-12 14:06:28 +0700

    t thêm đường dẫn r nhen còn cái css của login vs regis thì m tự sửa nhen Phong

--- views/main/layouts/home/navbar.php
+++ views/main/layouts/home/navbar.php
@@ -1,12 +1,12 @@
 
     <nav>
         <ul class="menu">
-            <li class="logo"><a href="#">BKBLUE</a></li>
-            <li class="item"><a href="#">Trang Chủ</a></li>
-            <li class="item"><a href="#">Chúng Tôi</a></li>
-            <li class="item"><a href="#">Sản Phẩm</a></li>
-            <li class="item button"><a href="#">Đăng Nhập</a></li>
-            <li class="item button secondary"><a href="#">Đăng Ký</a></li>
+            <li class="logo"><a href="/Source_ref">BKBLUE</a></li>
+            <li class="item"><a href="/Source_ref">Trang Chủ</a></li>
+            <li class="item"><a href="/Source_ref/views/main/about">Chúng Tôi</a></li>
+            <li class="item"><a href="/Source_ref/views/main/services">Sản Phẩm</a></li>
+            <li class="item button"><a href="/Source_ref/views/main/login">Đăng Nhập</a></li>
+            <li class="item button secondary"><a href="/Source_ref/views/main/register">Đăng Ký</a></li>
             <li class="toggle"><span class="bars"></span></li>
         </ul>
     </nav>
