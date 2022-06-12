dce5a2ada8b73a1cad7b729e0795df953f3f9c5a
hoangphu7122002 <phu.nguyen7122002hp@hcmut.edu.vn>
2022-06-12 10:45:58 +0700

    merge code Tan vao

---
+++ views/main/layout_Phong/hero.php
@@ -0,0 +1,38 @@
+
+<section id="hero">
+
+<div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+    <div class="carousel-inner container-slide">
+        <div class="carousel-item active">
+        <img class="d-block w-100" src="../../../public/images/bg-banner-1.jpg" alt="First slide">
+        <div class="carousel-caption d-none d-md-block">
+            <h1>Welcome to homepage</h1>
+            <div class="wrap">
+                <button class="button-effect">Get Started</button>
+            </div>
+        </div>
+        </div>
+        <div class="carousel-item">
+        <img class="d-block w-100" src="../../../public/images/bg-banner-1.jpg" alt="Second slide">
+        <div class="carousel-caption d-none d-md-block">
+            <h1>Second slide</h1>
+        </div>
+        </div>
+        <div class="carousel-item">
+        <img class="d-block w-100" src="../../../public/images/bg-banner-1.jpg" alt="Third slide">
+        <div class="carousel-caption d-none d-md-block">
+            <h1>Third slide</h1>
+        </div>
+        </div>
+    </div>
+    <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="sr-only">Previous</span>
+    </a>
+    <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="sr-only">Next</span>
+    </a>
+</div>
+
+</section>
--- views/main/layout_Phong/main.php
+++ views/main/layout_Phong/main.php
@@ -1,13 +1,15 @@
 <section id="about-us" >
-    <div class="container" data-aos="zoom-in"></div>
-        <div class="row content">
+    <div class="about-us" data-aos="zoom-in"></div>
+      <div class="section-title">
+          <h2>Về chúng tôi</h2>
+      </div>
+      <div class="row content">
           <div class="col-lg-6" data-aos="fade-right">
             <h2>Kiến tạo công nghệ và Phát triển con người. Vì một cuộc sống tốt đẹp hơn</h2>
-            <h3>VNGREEN tin vào sức mạnh thay đổi cuộc sống của Internet và không ngừng phát triển để mang đến cho người dùng những trải nghiệm ý nghĩa.</h3>
           </div>
           <div class="col-lg-6 pt-4 pt-lg-0" data-aos="fade-left">
             <p>
-              Thế mạnh của VNGREEN chính là con người và văn hóa doanh nghiệp. Dựa vào kim chỉ nam là 3 giá trị cốt lõi, các thành viên ở VNGREEN luôn có tinh thần cống hiến vì sự phát triển chung của VNGREEN và cộng đồng. Giá trị cốt lõi mà VNGREEN hướng tới là:
+              Thế mạnh của chính là con người và văn hóa doanh nghiệp. Dựa vào kim chỉ nam là 3 giá trị cốt lõi, các thành viên ở VNGREEN luôn có tinh thần cống hiến vì sự phát triển chung của VNGREEN và cộng đồng. Giá trị cốt lõi mà VNGREEN hướng tới là:
             </p>
             <ul>
               <li><i class="ri-check-double-line"></i> Đón Nhận Thách Thức</li>
@@ -23,13 +25,51 @@
 
 </section>
 
-<section id="services" class="section-bg">
+<section id="clients" class="clients">
+  <!-- use slick sáng làm--> 
+    <div class="clients-container" data-aos="fade-in">
+      <div class="section-title">
+          <h2>Các công ty đồng hành & tin tưởng</h2>
+      </div>
+      <div class="row center clients-wrap  ">
+        <div class="col-md-12 col-sm-12 col-xs-12 ">
+          <div class="client-logo">
+           <img alt="logo-company" class="img-fluid" src="/Web_MVC_Phong/public/images/clients/facebook.png">
+          </div>
+        </div>
+        <div class="col-md-12 col-sm-12 col-xs-12 ">
+          <div class="client-logo">
+           <img alt="logo-company" class="img-fluid" src="/Web_MVC_Phong/public/images/clients/google.png">
+          </div>
+        </div>
+        <div class="col-md-12 col-sm-12 col-xs-12 ">
+        <div class="client-logo">
+           <img alt="logo-company" class="img-fluid" src="/Web_MVC_Phong/public/images/clients/cj.png">
+          </div>
+        </div>
+        <div class="col-md-12 col-sm-12 col-xs-12 ">
+        <div class="client-logo">
+           <img alt="logo-company" class="img-fluid" src="/Web_MVC_Phong/public/images/clients/vtv.png">
+          </div>
+        </div>
+        <div class="col-md-12 col-sm-12 col-xs-12 ">
+        <div class="client-logo">
+           <img alt="logo-company" class="img-fluid" src="/Web_MVC_Phong/public/images/clients/marter_card.png">
+          </div>
+        </div>
+        <div class="col-md-12 col-sm-12 col-xs-12 ">
+        <div class="client-logo">
+           <img alt="logo-company" class="img-fluid" src="/Web_MVC_Phong/public/images/clients/hsbc.png">
+          </div>
+        </div>
+</section>
+
+<section id="services" class="services">
     <div class="container" data-aos="fade-up">
         <div class="section-title">
           <h2>Sản phẩm</h2>
           <p>Các lĩnh vực hoạt động của BKaV tập trung vào 4 nhóm sản phẩm chủ lực, mang đến cho người dùng những trải nghiệm phong phú và đơn giản hơn.</p>
         </div>
-
         <div class="row">
           <div class="col-lg-2 col-md-0"></div>
           <div class="col-lg-4 col-md-6 d-flex align-items-stretch mt-4" data-aos="zoom-in" data-aos-delay="100">
@@ -89,10 +129,6 @@
           <div class="col-lg-2 col-md-0"></div>
         
     </div>
-</section>
 
-<section id="clients">
-    <div class="clients-container" data-aos="zoom-in">
-        <h1>Hello</h1>
-    </div>
-</section>
+  </section>
+
