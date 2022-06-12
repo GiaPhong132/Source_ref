68010c02228b5d786dfc1a9550e6a33d0a7c4c83
GiaPhong132 <76869112+GiaPhong132@users.noreply.github.com>
2022-06-12 19:47:59 +0700

    edit include path in views/main/login/index.php

--- views/main/login/index.php
+++ views/main/login/index.php
@@ -33,7 +33,7 @@
 
 
 <body>
-    <?php include_once("../layouts/home/navbar.php");
+    <?php require_once("../navbar.php");
     ?>
     <div class="limiter">
         <div class="container-login100" style="background-image: url('/Source_ref/public2/images/tech-blocks.jpeg');">
