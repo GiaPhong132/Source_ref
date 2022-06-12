5bc7a2f456041fb758308544031e162d086fe964
hoangphu7122002 <phu.nguyen7122002hp@hcmut.edu.vn>
2022-06-12 14:14:21 +0700

    edit space

--- views/main/footer.php
+++ views/main/footer.php
@@ -1,84 +1,87 @@
  <!-- ======= Footer ======= -->
  <footer class="footer">
-  <div class="footer__addr">
-    <h1 class="footer__logo">
-      <img src="/MVC_Phong/public/images/hcmut.png" alt="logo">
-      BKaV</h1>
-      <h2>Contact</h2>
-      <address>Điện thoại: (84-24) 3763 2552, Email: Bkav@bkav.com.vn</address>
-      <address>Tòa nhà HH1 - Khu đô thị Yên Hòa - Phường Yên Hòa - Cầu Giấy - Hà Nội<br> 
-      <p>Giấy chứng nhận ĐKKD số 0101360697 do Sở Kế hoạch và đầu tư Thành Phố Hà Nội cấp lần đầu ngày 17/04/2003</p>
-      <a class="footer__btn" href="mailto:example@gmail.com">Email Us</a>
-    </address>
-  </div>
-  
-  <ul class="footer__nav">
-    <li class="nav__item">
-      <h2 class="nav__title">Chính sách</h2>
-      <ul class="nav__ul">
-        <li>
-          <a href="#">Điều khoản bảo mật</a>
-        </li>
+     <div class="footer__addr">
+         <h1 class="footer__logo">
+             <img src="/MVC_Phong/public/images/hcmut.png" alt="logo">
+             BKaV
+         </h1>
+         <h2>Contact</h2>
+         <address>Điện thoại: (84-24) 3763 2552, Email: Bkav@bkav.com.vn</address>
+         <address>Tòa nhà HH1 - Khu đô thị Yên Hòa - Phường Yên Hòa - Cầu Giấy - Hà Nội<br>
+             <p>Giấy chứng nhận ĐKKD số 0101360697 do Sở Kế hoạch và đầu tư Thành Phố Hà Nội cấp lần đầu ngày 17/04/2003</p>
+             <a class="footer__btn" href="mailto:example@gmail.com">Email Us</a>
+         </address>
+     </div>
 
-        <li>
-          <a href="#">Điều khoản sử dụng</a>
-        </li>
-            
-        <li>
-          <a href="#">Liên hệ</a>
-        </li>
-      </ul>
-    </li>
-    
-    <li class="nav__item ">
-      <h2 class="nav__title">Hỗ trợ</h2>
-      
-      <ul class="nav__ul ">
-        <li>
-          <a href="#">Hỗ trợ tài khoản</a>
-        </li>
-        
-        <li>
-          <a href="#">Hỗ trợ ứng dụng</a>
-        </li>
-      </ul>
-    </li>
-    
-    <li class="nav__item">
-      <h2 class="nav__title">Liên kết</h2>
-      
-      <ul class="nav__ul">
-        <li>
-          <a href="#">Facebook</a>
-        </li>
-        
-        <li>
-          <a href="#">Youtube</a>
-        </li>
-        
-        <li>
-          <a href="#">Instagram</a>
-        </li>
-      </ul>
-    </li>
-  </ul>
-  
-  <div class="legal">
-    <p>&copy; 1995 - 2022 Bkav Corporation. All Rights Reserved </p><br>
-    <p>Designed by BKaV</p>
-  </div>
-</footer><!-- End Footer -->
+     <ul class="footer__nav">
+         <li class="nav__item">
+             <h2 class="nav__title">Chính sách</h2>
+             <ul class="nav__ul">
+                 <li>
+                     <a href="#">Điều khoản bảo mật</a>
+                 </li>
 
-  <!-- <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a> -->
+                 <li>
+                     <a href="#">Điều khoản sử dụng</a>
+                 </li>
 
-  <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
-  <script>AOS.init();</script>  
-  
-  <script type="text/javascript" src="//code.jquery.com/jquery-1.11.0.min.js"></script>
-  <script type="text/javascript" src="//code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick.min.js" integrity="sha512-XtmMtDEcNz2j7ekrtHvOVR4iwwaD6o/FUJe6+Zq+HgcCsk3kj4uSQQR8weQ2QVj1o0Pk6PwYLohm206ZzNfubg==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
-  <script type="text/javascript" src="/Web_MVC_Phong/public/js/slick.js"></script>
+                 <li>
+                     <a href="#">Liên hệ</a>
+                 </li>
+             </ul>
+         </li>
 
-</body>
+         <li class="nav__item ">
+             <h2 class="nav__title">Hỗ trợ</h2>
 
-</html>
+             <ul class="nav__ul ">
+                 <li>
+                     <a href="#">Hỗ trợ tài khoản</a>
+                 </li>
+
+                 <li>
+                     <a href="#">Hỗ trợ ứng dụng</a>
+                 </li>
+             </ul>
+         </li>
+
+         <li class="nav__item">
+             <h2 class="nav__title">Liên kết</h2>
+
+             <ul class="nav__ul">
+                 <li>
+                     <a href="#">Facebook</a>
+                 </li>
+
+                 <li>
+                     <a href="#">Youtube</a>
+                 </li>
+
+                 <li>
+                     <a href="#">Instagram</a>
+                 </li>
+             </ul>
+         </li>
+     </ul>
+
+     <div class="legal">
+         <p>&copy; 1995 - 2022 Bkav Corporation. All Rights Reserved </p><br>
+         <p>Designed by BKaV</p>
+     </div>
+ </footer><!-- End Footer -->
+
+ <!-- <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a> -->
+
+ <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
+ <script>
+     AOS.init();
+ </script>
+
+ <script type="text/javascript" src="//code.jquery.com/jquery-1.11.0.min.js"></script>
+ <script type="text/javascript" src="//code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
+ <script src="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick.min.js" integrity="sha512-XtmMtDEcNz2j7ekrtHvOVR4iwwaD6o/FUJe6+Zq+HgcCsk3kj4uSQQR8weQ2QVj1o0Pk6PwYLohm206ZzNfubg==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+ <script type="text/javascript" src="/Web_MVC_Phong/public/js/slick.js"></script>
+
+ </body>
+
+ </html>
