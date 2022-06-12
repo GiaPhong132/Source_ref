483def199426033f15c0aa543805668563b35c12
hoangphu7122002 <phu.nguyen7122002hp@hcmut.edu.vn>
2022-06-12 14:25:21 +0700

    change include_once -> require_once in layouts/index.php

--- views/main/layouts/index.php
+++ views/main/layouts/index.php
@@ -1,11 +1,11 @@
-<?php include_once("views/main/header.php");?>
+<?php require_once("views/main/header.php"); ?>
 
-<?php include_once("home/navbar.php");?>
+<?php require_once("home/navbar.php"); ?>
 
-<?php include_once("home/hero.php");?>
-<?php include_once("home/about.php");?>
-<?php include_once("home/wedo.php");?>
-<?php include_once("home/clients.php");?>
-<?php include_once("home/services.php");?>
+<?php require_once("home/hero.php"); ?>
+<?php require_once("home/about.php"); ?>
+<?php require_once("home/wedo.php"); ?>
+<?php require_once("home/clients.php"); ?>
+<?php require_once("home/services.php"); ?>
 
-<?php include_once("views/main/footer.php");?>
+<?php require_once("views/main/footer.php"); ?>
