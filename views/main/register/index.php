bf23c9fd136ad2a7e03aaabcd5d3de4706b7f61a
hoangphu7122002 <phu.nguyen7122002hp@hcmut.edu.vn>
2022-06-12 13:28:39 +0700

    edit some include path to sync

--- views/main/register/index.php
+++ views/main/register/index.php
@@ -10,32 +10,32 @@
     <meta content="" name="keywords">
 
     <!-- Favicons -->
-    <link rel="icon" type="image/png" href="/SOURCE_REF/public2/images/icons/favicon.ico" />
+    <link rel="icon" type="image/png" href="/Source_ref/public2/images/icons/favicon.ico" />
     <!--===============================================================================================-->
-    <link rel="stylesheet" type="text/css" href="/SOURCE_REF/public2/vendor/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="/Source_ref/public2/vendor/bootstrap/css/bootstrap.min.css">
     <!--===============================================================================================-->
-    <link rel="stylesheet" type="text/css" href="/SOURCE_REF/public2/fonts/font-awesome-4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" type="text/css" href="/Source_ref/public2/fonts/font-awesome-4.7.0/css/font-awesome.min.css">
     <!--===============================================================================================-->
-    <link rel="stylesheet" type="text/css" href="/SOURCE_REF/public2/fonts/iconic/css/material-design-iconic-font.min.css">
+    <link rel="stylesheet" type="text/css" href="/Source_ref/public2/fonts/iconic/css/material-design-iconic-font.min.css">
     <!--===============================================================================================-->
-    <link rel="stylesheet" type="text/css" href="/SOURCE_REF/public2/vendor/animate/animate.css">
+    <link rel="stylesheet" type="text/css" href="/Source_ref/public2/vendor/animate/animate.css">
     <!--===============================================================================================-->
-    <link rel="stylesheet" type="text/css" href="/SOURCE_REF/public2/vendor/css-hamburgers/hamburgers.min.css">
+    <link rel="stylesheet" type="text/css" href="/Source_ref/public2/vendor/css-hamburgers/hamburgers.min.css">
     <!--===============================================================================================-->
-    <link rel="stylesheet" type="text/css" href="/SOURCE_REF/public2/vendor/animsition/css/animsition.min.css">
+    <link rel="stylesheet" type="text/css" href="/Source_ref/public2/vendor/animsition/css/animsition.min.css">
     <!--===============================================================================================-->
-    <link rel="stylesheet" type="text/css" href="/SOURCE_REF/public2/vendor/select2/select2.min.css">
+    <link rel="stylesheet" type="text/css" href="/Source_ref/public2/vendor/select2/select2.min.css">
     <!--===============================================================================================-->
-    <link rel="stylesheet" type="text/css" href="/SOURCE_REF/public2/vendor/daterangepicker/daterangepicker.css">
+    <link rel="stylesheet" type="text/css" href="/Source_ref/public2/vendor/daterangepicker/daterangepicker.css">
     <!--===============================================================================================-->
-    <link rel="stylesheet" type="text/css" href="/SOURCE_REF/public2/css/util.css">
-    <link rel="stylesheet" type="text/css" href="/SOURCE_REF/public2/css/main.css">
+    <link rel="stylesheet" type="text/css" href="/Source_ref/public2/css/util.css">
+    <link rel="stylesheet" type="text/css" href="/Source_ref/public2/css/main.css">
 </head>
 
 <body>
 
     <div class="limiter">
-        <div class="container-login100" style="background-image: url('/SOURCE_REF/public2/images/bg-01.jpg');">
+        <div class="container-login100" style="background-image: url('/Source_ref/public2/images/bg-01.jpg');">
             <div class="wrap-login100 p-l-55 p-r-55 p-t-65 p-b-54">
 
                 <form action="index.php?page=main&controller=register&action=submit" method="POST" class="login100-form validate-form">
@@ -125,7 +125,7 @@
                     </div>
                 </form>
 
-                <div class="login100-more" style="background-image: url('/SOURCE_REF/public2/assets/img/slide/VNG Campus 31.jpg');">
+                <div class="login100-more" style="background-image: url('/Source_ref/public2/assets/img/slide/VNG Campus 31.jpg');">
                 </div>
             </div>
 
