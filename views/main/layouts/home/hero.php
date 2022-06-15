f6283f093609a521a4358a5de073364047ab104d
Vinh <khanhvinhbui0512@gmail.com>
2022-06-15 16:07:08 +0700

    success home

--- views/main/layouts/home/hero.php
+++ views/main/layouts/home/hero.php
@@ -23,9 +23,8 @@
 		<div class="row align-items-center">
 			<div class="col-md-6 order-2 order-md-1 text-center text-md-left">
 				<h1 class="text-white font-weight-bold mb-4">Beautiful And Powful Evironment Development</h1>
-				<p class="text-white mb-5">Besides its beautiful design. Laapp is an incredibly rich core framework for you to
-					showcase your App.</p>
-				<a href="#" class="btn btn-main">Download Now</a>
+				<p class="text-white mb-5">A wide range of tools and integrations for any team</p>
+				<a href="#" class="btn btn-main">get started</a>
 			</div>
 			<div class="col-md-6 text-center order-1 order-md-2">
 				<img class="img-fluid" src="/source_ref/public2/images/visual.png" alt="screenshot">
