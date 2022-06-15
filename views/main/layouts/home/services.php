26f7e9403dcae9282c4b5e64d44da2d1cf0120a4
nhaTahn <74145826+nhaTahn@users.noreply.github.com>
2022-06-16 01:10:21 +0700

    fixed

--- views/main/layouts/home/services.php
+++ views/main/layouts/home/services.php
@@ -4,7 +4,7 @@
             <div class="col-lg-12">
                 <div class="section-title">
                     <h2>An Interface Of Our IDE</h2>
-                    <p>Industry-standard adapter for connecting IDE devices and enabling them to communicate.</p>
+                    <p style="text-align:center;">Industry-standard adapter for connecting IDE devices and enabling them to communicate.</p>
                 </div>
             </div>
         </div>
