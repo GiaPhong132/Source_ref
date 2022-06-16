e51315e8f11b0106c6862d3450b659f09ba7856c
GiaPhong132 <76869112+GiaPhong132@users.noreply.github.com>
2022-06-17 02:36:32 +0700

    upload ok, còn bước hiện hình ảnh

--- controllers/main/profile_controller.php
+++ controllers/main/profile_controller.php
@@ -8,23 +8,51 @@ class ProfileController extends BaseController
         $this->folder = 'profile';
     }
 
-<<<<<<< HEAD
     public function index()
     {
         $this->render('index');
     }
 
-    public function editProfile()
+
+    public function editInfo()
     {
-        $fname = $_POST['first_name'];
-        $lname = $_POST['last_name'];
-        $phone = $_POST['phone_number'];
-        $email = $_POST['email'];
+        session_start();
+        $email = $_SESSION['guest'];
+        $fname = $_POST['fname'];
+        $lname = $_POST['lname'];
+        $gender = $_POST['gender'];
+        $age = $_POST['age'];
+        $phone = $_POST['phone'];
+        $urlcurrent = $_POST['img'];
+        // Photo
+        $target_dir = "public/img/user/";
+        $path = $_FILES['fileToUpload']['name'];
+        $ext = pathinfo($path, PATHINFO_EXTENSION);
+        $id = (string)date("Y_m_d_h_i_sa");
+        $fileuploadname = (string)$id;
+        $fileuploadname .= ".";
+        $fileuploadname .= $ext;
+        $target_file = $target_dir . basename($fileuploadname);
+        if (file_exists($target_file)) {
+            echo "Sorry, file already exists.";
+        }
+        $fileType = strtolower(pathinfo($target_file, PATHINFO_EXTENSION));
+        // Allow certain file formats
+        if (
+            $fileType != "jpg" && $fileType != "png" && $fileType != "jpeg"
+            && $fileType != "gif"
+        ) {
+            echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
+            $upload_ok = 0;
+        }
+        if ($_FILES["fileToUpload"]["size"] > 500000) {
+            echo "Sorry, your file is too large.";
+        }
+        $file_pointer = $urlcurrent;
+        unlink($file_pointer);
+        move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file);
+        // Update
+        $change_info = User::update($email, $target_file, $fname, $lname, $gender, $age, $phone);
+        header('Location: index.php?page=main&controller=layouts&action=index');
     }
-=======
-	public function index()
-	{
-		$this->render('index');
-	}
->>>>>>> bd1246df063a8e5305d519c36d90aff4c849d617
 }
