c55c8dd400165eb6d775a933fe508ebd1c6d5f22
GiaPhong132 <76869112+GiaPhong132@users.noreply.github.com>
2022-06-17 17:00:19 +0700

    change profile-> filetoupload

--- views/main/profile/index.php
+++ views/main/profile/index.php
@@ -4,7 +4,30 @@ require_once("/xampp/htdocs/Source_code/views/main/navbar.php");
 
 ?>
 
+<!DOCTYPE html>
+<html lang="en">
 
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+
+
+    <style>
+        img {
+            border-radius: 50%;
+            width: 200px;
+            height: 200px;
+        }
+    </style>
+</head>
+
+<body>
+
+</body>
+
+</html>
 
 
 <div class="container rounded bg-white mt-5 mb-5">
