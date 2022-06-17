e3ee57a5bb26c5fe708651d432a362b173593405
Vinh <khanhvinhbui0512@gmail.com>
2022-06-17 16:02:21 +0700

    member cap nhat

--- views/admin/header.php
+++ views/admin/header.php
@@ -13,11 +13,13 @@
     <link rel="stylesheet" href="/Source_code/public/plugins/datatables-bs4/css/dataTables.bootstrap4.min.css">
     <link rel="stylesheet" href="/Source_code/public/plugins/datatables-responsive/css/responsive.bootstrap4.min.css">
     <link rel="stylesheet" href="/Source_code/public/plugins/datatables-buttons/css/buttons.bootstrap4.min.css">
+    <link href="/Source_code/public2/vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
     <!-- Theme style-->
     <link rel="stylesheet" href="/Source_code/public/dist/css/adminlte.min.css">
     <link rel="stylesheet" href="/Source_code/public2/css/admin.css">
-    <script type="text/javascript" src="/Source_code/public2/js/admin.js"></script>
+    <script type="text/javascript" src="/Source_code/public2/css/admin.js"></script>
     <script type="text/javascript" src="/Source_code/public2/vendor/jquery/jquery.js"></script>
     <script type="text/javascript" src="/Source_code/public2/vendor/bootstrap/js/bootstrap.bundle.js"></script>
     <script type="text/javascript" src="/Source_code/public2/vendor/jquery-easing/jquery.easing.js"></script>
     <script type="text/javascript" src="/Source_code/public2/vendor/chart.js/Chart.js"></script>
+    
--- views/admin/members/index.php
+++ views/admin/members/index.php
@@ -19,7 +19,7 @@ require_once('/xampp/htdocs/Source_code/views/admin/content_layouts.php'); ?>
 
 <!-- Code -->
 
-<div class="content-wrapper">
+<div id="content-wrapper" class="d-flex flex-column">
     <!-- Add Content -->
     <!-- Content Header (Page header)-->
     <section class="content-header">
@@ -172,13 +172,14 @@ require_once('/xampp/htdocs/Source_code/views/admin/content_layouts.php'); ?>
 
     </section>
 </div>
-
+</div
 
 <?php
 require_once('/xampp/htdocs/Source_code/views/admin/footer.php'); ?>
 
 <!-- Add Javascripts -->
 <script src="/Source_code/public/js/admin/index.js"></script>
+<script type="text/javascript" src="/Source_code/public2/js/admin.js"></script>
 </body>
 
 </html>
