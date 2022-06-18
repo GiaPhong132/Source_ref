d30d51e6c0d2fe4f07c7864796f65afedeb1c58f
Vinh <khanhvinhbui0512@gmail.com>
2022-06-18 13:14:02 +0700

    update description

--- database.sql
+++ database.sql
@@ -39,29 +39,76 @@ CREATE TABLE `product` (
 --
 
 INSERT INTO `product` (`id`, `price`, `name`, `description`, `content`, `img`) VALUES
-(1, 2900, 'Visual Studio Code', 'Powerful', 'Đây là một sản phẩm đến từ BKBLUE', 'public2/images/product/visual_studio_code.png');
+(1, 2900, 'Visual Studio Code', 'Powerful', 'Visual Studio Code is a lightweight but powerful
+source code editor which runs on your desktop and is available for Windows, macOS and Linux. 
+It comes with built-in support for JavaScript, TypeScript and Node.js and has a rich ecosystem
+ of extensions for other languages (such as C++, C#, Java, Python, PHP, Go) and runtimes 
+ (such as .NET and Unity).', 'public2/images/product/visual_studio_code.png');
+
 INSERT INTO `product` (`id`, `price`, `name`, `description`, `content`, `img`) VALUES
-(2, 5900, 'Docker', 'Powerful', 'Đây là một sản phẩm đến từ BKBLUE', 'public2/images/product/docker.png');
+(2, 5900, 'Docker', 'Powerful', 'Docker is a Linux-based, open-source containerization platform that developers 
+use to build, run, and package applications for deployment using containers. Unlike virtual machines, Docker 
+containers offer: OS-level abstraction with optimum resource utilization.', 'public2/images/product/docker.png');
+
+
 INSERT INTO `product` (`id`, `price`, `name`, `description`, `content`, `img`) VALUES
-(3, 3300, 'PhpStorm', 'Powerful', 'Đây là một sản phẩm đến từ BKBLUE', 'public2/images/product/phpstorm.png');
+(3, 3300, 'PhpStorm', 'Powerful', 'PhpStorm is an Integrated Development Environment for PHP developers built 
+on top of the IntelliJ IDEA platform. JetBrains PhpStorm is an innovative and cross-platform IDE that become 
+popular over the last couple of year. It is perfect for working with Drupal, Symfony, Laravel, WordPress, 
+Zend Framework, Joomla, CakePHP, and other frameworks.', 'public2/images/product/phpstorm.png');
+
+
 INSERT INTO `product` (`id`, `price`, `name`, `description`, `content`, `img`) VALUES
-(4, 4800, 'Jupyter', 'Powerful', 'Đây là một sản phẩm đến từ BKBLUE', 'public2/images/product/jupyter.png');
+(4, 4800, 'Jupyter', 'Powerful', 'The Jupyter Notebook is an open source web application that you can 
+use to create and share documents that contain live code, equations, visualizations, and text. Jupyter 
+Notebook is maintained by the people at Project Jupyter.', 'public2/images/product/jupyter.png');
+
+
 INSERT INTO `product` (`id`, `price`, `name`, `description`, `content`, `img`) VALUES
-(5, 2100, 'Vim', 'Powerful', 'Đây là một sản phẩm đến từ BKBLUE', 'public2/images/product/vim.png');
+(5, 2100, 'Vim', 'Powerful', 'Vim is a highly configurable text editor built to make creating and changing 
+any kind of text very efficient. It is included as "vi" with most UNIX systems and with Apple OS X. Vim is 
+rock stable and is continuously being developed to become even better.', 'public2/images/product/vim.png');
+
+
 INSERT INTO `product` (`id`, `price`, `name`, `description`, `content`, `img`) VALUES
-(6, 7200, 'Eclipse', 'Powerful', 'Đây là một sản phẩm đến từ BKBLUE', 'public2/images/product/eclipse.png');
+(6, 7200, 'Eclipse', 'Powerful', 'Eclipse is an integrated development environment (IDE) for 
+Java and other programming languages like C, C++, PHP, and Ruby etc. Development environment 
+provided by Eclipse includes the Eclipse Java development tools (JDT) for Java, Eclipse CDT 
+for C/C++, and Eclipse PDT for PHP, among others.', 'public2/images/product/eclipse.png');
+
+
 INSERT INTO `product` (`id`, `price`, `name`, `description`, `content`, `img`) VALUES
-(7, 4000, 'Sublime', 'Powerful', 'Đây là một sản phẩm đến từ BKBLUE', 'public2/images/product/sublime.png');
+(7, 4000, 'Sublime', 'Powerful', 'Sublime Text Editor is a full featured Text editor for 
+editing local files or a code base. It includes various features for editing code base 
+which helps developers to keep track of changes.', 'public2/images/product/sublime.png');
+
+
 INSERT INTO `product` (`id`, `price`, `name`, `description`, `content`, `img`) VALUES
 (8, 1000, 'Atom', 'Powerful', 'Đây là một sản phẩm đến từ BKBLUE', 'public2/images/product/atom.png');
+
+
 INSERT INTO `product` (`id`, `price`, `name`, `description`, `content`, `img`) VALUES
-(9, 4000, 'PyCharm', 'Powerful', 'Đây là một sản phẩm đến từ BKBLUE', 'public2/images/product/pycharm.png');
+(9, 4000, 'PyCharm', 'Powerful', 'PyCharm is a dedicated Python Integrated Development Environment (IDE) 
+providing a wide range of essential tools for Python developers, tightly integrated to create a convenient 
+environment for productive Python, web, and data science development.', 'public2/images/product/pycharm.png');
+
+
 INSERT INTO `product` (`id`, `price`, `name`, `description`, `content`, `img`) VALUES
-(10, 8000, 'Android-Studio', 'Powerful', 'Đây là một sản phẩm đến từ BKBLUE', 'public2/images/product/androidstudio.png');
+(10, 8000, 'Android-Studio', 'Powerful', 'Android Studio is an IDE for BKBLUE launched on 16th May 2013
+, during BKBLUE I/O 2013 event. Android Studio contains all the Android tools to design, test, debug, 
+and profile your application.', 'public2/images/product/androidstudio.png');
+
+
 INSERT INTO `product` (`id`, `price`, `name`, `description`, `content`, `img`) VALUES
-(11, 7000, 'IntelliJ', 'Powerful', 'Đây là một sản phẩm đến từ BKBLUE', 'public2/images/product/intellij.png');
+(11, 7000, 'IntelliJ', 'Powerful', 'IntelliJ is one of the most powerful and popular Integrated Development Environments 
+(IDE) for Java. It is developed and maintained by JetBrains and available as community and ultimate edition. This feature 
+rich IDE enables rapid development and helps in improving code quality.', 'public2/images/product/intellij.png');
+
+
 INSERT INTO `product` (`id`, `price`, `name`, `description`, `content`, `img`) VALUES
-(12, 6000, 'Visual Studio', 'Powerful', 'Đây là một sản phẩm đến từ BKBLUE', 'public2/images/product/visualstudio.png');
+(12, 6000, 'Visual Studio', 'Powerful', 'To develop any type of app or learn a language, you’ll be working in the 
+Visual Studio Integrated Development Environment (IDE). Beyond code editing, Visual Studio IDE brings together 
+graphical designers, extensions and many more features in one place. ', 'public2/images/product/visualstudio.png');
 
 
 -- --------------------------------------------------------
