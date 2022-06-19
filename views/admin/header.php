867e0879761dfec16b96b07a5746ebbc7b67ec9b
GiaPhong132 <76869112+GiaPhong132@users.noreply.github.com>
2022-06-19 13:55:01 +0700

    .

--- views/admin/header.php
+++ views/admin/header.php
@@ -8,14 +8,14 @@
     <!-- Google Font: Source Sans Pro-->
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&amp;display=fallback">
     <!-- Font Awesome-->
-    <link rel="stylesheet" href="/Source_code/public/plugins/fontawesome-free/css/all.min.css">
+    <link rel="stylesheet" href="/Source_code/public2/plugins/fontawesome-free/css/all.min.css">
     <!-- DataTables-->
-    <link rel="stylesheet" href="/Source_code/public/plugins/datatables-bs4/css/dataTables.bootstrap4.min.css">
-    <link rel="stylesheet" href="/Source_code/public/plugins/datatables-responsive/css/responsive.bootstrap4.min.css">
-    <link rel="stylesheet" href="/Source_code/public/plugins/datatables-buttons/css/buttons.bootstrap4.min.css">
+    <link rel="stylesheet" href="/Source_code/public2/plugins/datatables-bs4/css/dataTables.bootstrap4.min.css">
+    <link rel="stylesheet" href="/Source_code/public2/plugins/datatables-responsive/css/responsive.bootstrap4.min.css">
+    <link rel="stylesheet" href="/Source_code/public2/plugins/datatables-buttons/css/buttons.bootstrap4.min.css">
     <link href="/Source_code/public2/vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
     <!-- Theme style-->
-    <link rel="stylesheet" href="/Source_code/public/dist/css/adminlte.min.css">
+    <link rel="stylesheet" href="/Source_code/public2/dist/css/adminlte.min.css">
     <link rel="stylesheet" href="/Source_code/public2/css/admin.css">
     <script type="text/javascript" src="/Source_code/public2/css/admin.js"></script>
     <script type="text/javascript" src="/Source_code/public2/vendor/jquery/jquery.js"></script>
